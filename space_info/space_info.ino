5d8f5f9798b97487ec27b2ba7c0feff3d42a1203
H3wastooshort <hacker3000@posteo.org>
2023-07-27 22:55:38 +0200

    first steps

---
+++ space_info/space_info.ino
@@ -0,0 +1,217 @@
+/*
+Compile with minimal SPIFFS Partition Scheme
+
+Layout is grid of blocks. Each block has an icon and text. Status is shown as color and in text form.
+
+Homeassistant sends states via REST. Uses jinja templating of homeassistant to insert states.
+*/
+
+#include <ESP32Lib.h>
+#include <WiFiManager.h>
+#include <ArduinoJson.h>
+#include <Ressources/CodePage437_8x14.h>
+#include <WebServer.h>
+#include <ArduinoOTA.h>
+#include <map>
+#include <LittleFS.h>
+
+#include "sprites.h"
+#include "stuff.h"
+
+VGA3Bit vga;
+WebServer srv;
+
+auto color_ok = vga.RGB(0, 255, 255);
+auto color_bad = vga.RGB(255, 0, 255);
+auto color_unknown = vga.RGB(255, 255, 0);
+auto white = vga.RGB(255, 255, 255);
+
+const int redPin = 14;
+const int greenPin = 19;
+const int bluePin = 27;
+const int hsyncPin = 32;
+const int vsyncPin = 33;
+
+#define BLOCK_SIZE 128  //each block is 128x128
+String layout_path = "/layout.json";
+
+DynamicJsonDocument layout(8192);
+std::map<String, uint8_t> states{};
+
+auto st_to_col(uint8_t s) {
+  switch (s) {
+    default: return color_unknown; break;
+    case 1: return color_bad; break;
+    case 2: return color_ok; break;
+  }
+}
+
+
+/*
+XY Block-Koordinaten,
+Beschreibung 15x3 Zeichen,
+Icon
+
+{
+  "test_block":{
+    "x":2, "y":2,
+    "desc": ["Test","Block",""],
+    "icon": "ha"
+  }
+}
+*/
+
+void draw_block(String name) {
+  if (!layout.containsKey(name)) return;
+
+  uint16_t x = layout[name]["x"] * BLOCK_SIZE;
+  uint16_t y = layout[name]["y"] * BLOCK_SIZE;
+  auto c = st_to_col(states[name]);
+
+  //Draw rect
+  vga.rect(x, y, BLOCK_SIZE, BLOCK_SIZE, c);
+
+  //Draw icon
+  if (layout[name].containsKey("icon")) {
+    uint8_t id = name_to_sprite[layout[name]["icon"]];
+    //expecting 64x64 icons
+    if (id < 255) sprites.drawMix(vga, id, x + 32, y + 2);
+  }
+
+  //Draw description
+  vga.setTextColor(c);
+  for (uint8_t l = 0; l < 3; l++) {
+    vga.setCursor(x, y + 80 + (l * 16));
+    vga.print(layout[name]["desc"][l]);
+  }
+  vga.setTextColor(white);
+}
+
+void draw_all_blocks() {
+  JsonObject root = doc.as<JsonObject>();
+  for (JsonPair kv : root) {
+    draw_block(kv.key());
+  }
+}
+
+
+/*
+{ "binary_sensor.window_1: "on",
+  "binary_sensor.window_2": off }
+*/
+void handle_state_update() {
+  if (srv.hasArg("plain")) {
+    String json = srv.arg("plain");
+    DynamicJsonDocument doc(4096);
+    auto err = deserializeJson(layout, json);
+    if (err == DeserializationError::Ok) {
+
+      JsonObject root = doc.as<JsonObject>();
+      for (JsonPair kv : root) {
+        String jval = kv.value();
+        jval.toLowerCase();
+        uint8_t val;
+        if (jval == "on") val = 2;
+        else if (jval == "off") val = 1;
+        else val = 0;
+        states[kv.key().as<String>()] = val;
+      }
+      draw_all_blocks();
+
+      srv.send(200, "application/json", "{\"success\":\"states set\"}");
+    } else {
+      String err_txt = "{\"error\":\"";
+      err_txt += err.f_str();
+      err_txt += "\"}";
+      srv.send(400, "application/json", err_txt);
+    }
+  } else srv.send(400, "application/json", "{\"error\":\"no data\"}");
+}
+
+void handle_layout() {
+  if (srv.hasArg("plain")) {
+    String json = srv.arg("plain");
+    auto err = deserializeJson(layout, json);
+    if (err == DeserializationError::Ok) {
+      File lf = LittleFS.open(layout_path, FILE_WRITE);
+      lf.print(json);
+      lf.close();
+      srv.send(200, "application/json", "{\"success\":\"new layout set\"}");
+    } else {
+      String err_txt = "{\"error\":\"";
+      err_txt += err.f_str();
+      err_txt += "\"}";
+      srv.send(400, "application/json", err_txt);
+    }
+  } else srv.send(400, "application/json", "{\"error\":\"no data\"}");
+}
+
+void setup() {
+  Serial.begin(115200);
+  Serial.print("\nvspace.one Info-Monitor, compiled ");
+  Serial.println(__DATE__);
+
+  //VGA
+  Serial.println("VGA...");
+  Mode monitor_res = vga.MODE800x600.custom(640, 512);
+  vga.init(monitor_res, redPin, greenPin, bluePin, hsyncPin, vsyncPin);
+  vga.setFont(CodePage437_8x8);
+  vga.clear(0);
+
+  //WiFi
+  Serial.println("WiFi... ");
+  vga.clear(0);
+  vga.print("WiFi... ");
+  WiFiManager wm;
+  wm.setConfigPortalTimeout(180);
+  WiFi.hostname("space_info");
+  wm.setAPCallback(wm_ap_c);
+  if (!wm.autoConnect(conf_ssid, conf_pass)) {
+    ESP.restart();
+  }
+  vga.println("OK");
+
+  //OTA
+  Serial.println("OTA... ");
+  vga.clear(0);
+  vga.print("OTA... ");
+  init_vga_ota();
+  ArduinoOTA.setHostname("space_info");
+  ArduinoOTA.begin();
+  vga.println("OK");
+
+  //LittleFS
+  Serial.println("LittleFS... ");
+  vga.clear(0);
+  vga.print("LittleFS... ");
+  if (!LittleFS.begin(true)) {
+    Serial.println("ERROR");
+    vga.println("ERROR");
+  } else vga.println("OK");
+
+  //REST Server
+  Serial.println("REST... ");
+  vga.clear(0);
+  vga.print("REST... ");
+  srv.on("/", []() {
+    srv.send(200, "text/plain", "API Documentation at: [TODO]");
+  });
+  srv.on("/layout", HTTP_POST, handle_layout);
+  srv.on("/layout", HTTP_GET, []() {
+    uint32_t size = measureJson(layout) + 1;
+    char buf[size];
+    serializeJson(layout, buf);
+    srv.send(200, "application/json", buf, size);
+  });
+  srv.on("/layout", HTTP_POST, handle_state_update);
+  srv.begin();
+  vga.println("OK");
+
+  vga.clear(0);
+  draw_all_blocks();
+}
+
+void loop() {
+  srv.handleClient();
+  delay(5);
+}
---
+++ space_info/stuff.h
@@ -0,0 +1,88 @@
+std::map<String, uint8_t> name_to_sprite{
+  { "none", 255},
+  { "ha", 0}
+};
+
+void init_vga_ota() {
+  ArduinoOTA
+    .onStart([]() {
+      String ota_type;
+      if (ArduinoOTA.getCommand() == U_FLASH)
+        ota_type = "sketch";
+      else  // U_SPIFFS
+        ota_type = "filesystem";
+
+      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+      vga.clear(vga.RGB(0, 0, 255));
+      vga.backColor = vga.RGB(0, 0, 255);
+      vga.setTextColor(vga.RGB(255, 255, 255));
+      vga.setCursor(1, 1);
+      vga.println("Starting OTA...");
+      vga.print("OTA Type: ");
+      vga.println(ota_type.c_str());
+      vga.show();
+      Serial.println("Start updating " + ota_type);
+      delay(1000);
+    })
+    .onEnd([]() {
+      vga.clear(vga.RGB(0, 255, 0));
+      vga.backColor = vga.RGB(0, 255, 0);
+      vga.setTextColor(vga.RGB(255, 255, 255));
+      vga.setCursor(1, 1);
+      vga.println("OTA Successful!");
+      vga.show();
+      Serial.println("\nEnd");
+      delay(1000);
+    })
+    .onProgress([](unsigned int progress, unsigned int total) {
+      static bool set_color = false;
+      if (!set_color) {
+        vga.clear(vga.RGB(255, 0, 255));
+        vga.backColor = vga.RGB(255, 0, 255);
+        vga.setTextColor(vga.RGB(255, 255, 255));
+      } else {
+        vga.fillRect(1, 1, 320, 8, vga.RGB(255, 0, 255));
+      }
+      set_color = true;
+      vga.setCursor(1, 1);
+      vga.print("OTA Download Progress: ");
+      vga.print(progress / (total / 100));
+      vga.println("%");
+
+      vga.rect(0, 100, 320, 10, vga.RGB(255, 255, 255));
+      vga.fillRect(2, 102, map(progress / (total / 100), 0, 100, 2, 316), 6, vga.RGB(255, 255, 0));
+
+      vga.setCursor(0, 112);
+      vga.print("0%");
+      vga.setCursor(284, 112);
+      vga.print("100%");
+
+      vga.show();
+
+      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    })
+    .onError([](ota_error_t error) {
+      vga.clear(vga.RGB(255, 0, 0));
+      vga.backColor = vga.RGB(255, 0, 0);
+      vga.setTextColor(vga.RGB(255, 255, 255));
+      vga.setCursor(1, 1);
+      vga.println("OTA ERROR!");
+      vga.println(error);
+      if (error == OTA_AUTH_ERROR) vga.println("Auth Failed");
+      else if (error == OTA_BEGIN_ERROR) vga.println("Begin Failed");
+      else if (error == OTA_CONNECT_ERROR) vga.println("Connect Failed");
+      else if (error == OTA_RECEIVE_ERROR) vga.println("Receive Failed");
+      else if (error == OTA_END_ERROR) vga.println("End Failed");
+
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+      else if (error == OTA_END_ERROR) Serial.println("End Failed");
+
+      vga.show();
+
+      delay(5000);
+    });
+}
