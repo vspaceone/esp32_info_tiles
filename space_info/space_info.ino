742d114a668432d046e1cfa9de1ea902d8a02afb
HACKER3000 <github@hacker3000.cf>
2024-08-15 20:58:11 +0200

    fix POST

--- space_info/space_info.ino
+++ space_info/space_info.ino
@@ -220,7 +220,7 @@ void setup() {
 #else
     http.begin(bootup_request_data_webhook);
 #endif
-    const int code = http.POST();
+    const int code = http.POST(hostname);
     Serial.print(code);
     vga.print(code);
     if (code == HTTP_CODE_OK) {
